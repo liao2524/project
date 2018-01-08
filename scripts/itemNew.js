dd28f1902004e15594080b5240040adf93df8b04
liao <576952941@qq.com>
2018-01-08 21:50:08 +0800

    20180108

--- scripts/itemNew.js
+++ scripts/itemNew.js
@@ -33,6 +33,7 @@ var imgAfterSrc = "images/item/itemIdea.gif";
 var imgJoinSrc = "images/item/focus.jpeg";
 var imgJoinRightSrc = "";
 var imgJoinLeftSrc = "images/item/itemFocus_left.png";
+var imgLargeSrc="images/item/itemGucci.jpg";
 function createTagItem(type){
 	if (300 === type) {
 		var imgAfter = '<img src="'+imgAfterSrc+'">';
@@ -41,13 +42,14 @@ function createTagItem(type){
 
 		var JoinTextStr = '这是一段文字描述这是一段文字描述这是一段文字描述这是一段文字描述';
 		var joinH3Str = "【标题标题标题标】";
-		var h3Tag = '<h3>'+joinH3Str+'<h3>';
+		var h3Tag = '<h3>'+joinH3Str+'</h3>';
 		var pTag = '<p>'+ JoinTextStr+'</p>';
 		var divBottom  = '<div class="item_join_bottom">'+ h3Tag + pTag+'</div>';
 
 		var imgJoin = '<img src="'+imgJoinSrc+'"  class="item_join_img">';
 
-		var	imgJoinRight = '<img src="'+imgJoinRightSrc+'"  class="item_join_img">';
+		/*var	imgJoinRight = '<img src="'+imgJoinRightSrc+'"  class="item_join_right_img">';*/
+		var	imgJoinRight ='';
 		var joinRight = '<div class="item_join_right">'+ imgJoinRight+'</div>';
 
 		var	imgJoinLeft = '<img src="'+imgJoinLeftSrc+'"  class="item_join_left">';
@@ -58,7 +60,7 @@ function createTagItem(type){
 		// 由最里层开始添加
 		var commentStr = '<span>'+commentCount+'</span>';
 		var praiseStr = '<span>'+praiseCount+'</span>';	
-		var divTimeCommentPraise = '<div>'+ commentStr + praiseStr + '</div>';
+		var divTimeCommentPraise = '<div class="item_date_ribbon">'+ commentStr + praiseStr + '</div>';
 		var timeStr = '<span>刚刚</span>';
 		var itemDate = '';
 		
@@ -70,23 +72,26 @@ function createTagItem(type){
 		}
 
 		var descStr = '这是一段文字描述';
-		var descLargeStr = '这是一段大图的文字描述';
+		var descLargeStr = '这是一个大标题这是一个大标题这是一个大标题这是一个大标题这是一个大标题';
 		var divTxt = '<div class="item_text">'+descStr+'</div>';
 
-		var divLargeTxt = '<div class="item_large_text">'+descLargeStr+'</div>';
+		var divLargeTxt = '<div class="item_large_text"> <h3> <span>'+descLargeStr+' </h3> </span></div>';
 
 		var img = '<img src="'+imgSrc+'">';
+		var imgLarge='<img src="'+imgLargeSrc+'">';
 		var categoryStr = '<span>商业</span>';
-		var divCategory = '<div class="item_img">'+categoryStr+img+'</div>';
+		var divItemImg = '';
 
 		var aTag = '';
 		if(100 === type){
-			aTag = '<a href="item.html">'+divCategory+divTxt+itemDate+'</a>';
+			divItemImg = '<div class="item_img">'+img+categoryStr+'</div>';
+			aTag = '<a href="item.html">'+divItemImg+divTxt+itemDate+'</a>';
 			return '<div class="item_content">'+aTag+'</div>';
 		} 
 		if(200 === type){
-			aTag = '<a href="item.html">'+divCategory+divLargeTxt+itemDate+'</a>';
-			return '<div class="item_content">'+aTag+'</div>';
+			divItemImg = '<div class="item_large_img">'+imgLarge+categoryStr+'</div>';
+			aTag = '<a href="item.html">'+divItemImg+divLargeTxt+itemDate+'</a>';
+			return '<div class="item_content_large">'+aTag+'</div>';
 		} 
 	}
 
@@ -102,18 +107,55 @@ $item.append($itemContent);
 */
 $(function(){
 	// createTagItem();
-	divCount(1,0);
+	divCount(16,0);
 });
+
 var scrollCount=0;
-
-
 function divCount(TagCount,scrollCount) {
-	if (scrollCount<2) {
+	// alert(scrollCount);
+	/*var $itemContent =createTagItem(100);*/
+	var $itemContent ='';
+	var $item = $('.itemLear');
+	var sign = 0;
+	/*var typeVal = Math.random();*/
+	if (scrollCount===0) {
+		for (var j = 0; j < 5; j++) {
+			if (j===4) {
+				$itemContent=createTagItem(300);
+			}else{
+				$itemContent=createTagItem(100);
+			}
+			$item.before($itemContent);
+		}
+	scrollCount+=1;
+	TagCount-=5;
+	}
+	/*alert(typeVal);*/
+	/*var typeVal=0.5;*/
+	if (0<scrollCount<3) {
 		for (var i = 0; i < TagCount; i++) {
-			var $itemContent = createTagItem(300);
-			var $item = $('.itemLear');
+			var typeVal = Math.random();
+			var r = sign%4;
+			console.log(r);
+			// alert(sign);
+			// alert(typeof typeVal);
+			// console.log(typeVal);
+			if (typeVal<=0.8) {
+				$itemContent=createTagItem(100);
+				sign += 1;
+			}else{
+				if (sign==3) {
+					$itemContent=createTagItem(100);
+					sign += 1;
+				}else{
+					$itemContent=createTagItem(200);
+					sign += 2;
+				}
+			}
+			
+			// console.log(sign);
+			
 			$item.before($itemContent);	
-			// createTagItem(100);
 		}	
 	}else{
 		$('.showmore').show();
@@ -122,7 +164,7 @@ function divCount(TagCount,scrollCount) {
 $(function () {
 	$('.showmore_btn').click(function() {
 		divCount(10,0);
-		scrollCount=0;
+		scrollCount=1;
 	});
 });
 $(function () {
@@ -131,9 +173,9 @@ $(function () {
 		var contentHeigth = $('#content').height()-483;
 		
 		if (scrollTop>=contentHeigth) {
-			setTimeout(divCount(10,scrollCount),3000);
-			
-			scrollCount+=1;
+			divCount(10,1);
+			/*setTimeout(divCount(10,scrollCount),3000);*/
+			scrollCount+=1;	
 		}
 		$(".allNav_list").css('display', 'none');
 	});	
